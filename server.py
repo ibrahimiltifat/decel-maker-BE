e18071e99d4a29cc114a3756d64cd853f9e7ae58
mi014378 <38471029+mi014378@users.noreply.github.com>
2022-02-15 22:07:04 +0500

    make it better

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from flask_cors import CORS,cross_origin
 app=Flask(__name__)
 
 cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods = ['GET','POST'])
