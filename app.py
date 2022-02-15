d5ba785adbd412eda8a017b4b016f1ad0e85a7ba
mi014378 <38471029+mi014378@users.noreply.github.com>
2022-02-15 22:19:59 +0500

    make it better

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import jsonify
 from flask_cors import CORS,cross_origin
 
 app=Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
 @app.route('/', methods = ['GET','POST'])
